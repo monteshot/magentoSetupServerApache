19d676a7fd30df676d3eb0a1c3ec21300483db2f
Sergey Kusch <faqreg@gmail.com>
2020-04-13 14:26:26 +0300

    Only one-stop script command to generate apache2 site

--- createM2instance.sh
+++ createM2instance.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/$1.key -out /etc/ssl/certs/$1.crt
+DOMENNAME=$1
+echo "UA
+Sumy Region
+Shostka
+MonteShot
+MonteShot
+$1
+monteshot@monteshot.com" | sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/$1.key -out /etc/ssl/certs/$1.crt
 mkdir -p ~/ApacheSites/$1/log/
 mkdir -p ~/ApacheSites/$1/content/
 DOMENNAME=$1
 sites_available="/etc/apache2/sites-available/"
 tempdir="/var/tmp/sites4apache/"
+echo "127.0.0.1 $1" | sudo tee -a /etc/hosts
 mkdir -p $tempdir
 sudo ./genSite.sh $DOMENNAME > $tempdir$1.conf
 sudo cp $tempdir$1.conf $sites_available$1.conf
